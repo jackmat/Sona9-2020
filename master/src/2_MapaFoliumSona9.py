c7b7c71eb971a4c8180c7d30f9e837ea1ed82e5e
jackmat <csansfuentes@gmail.com>
2020-05-19 13:13:50 +0200

    legend and text band name added

--- master/src/2_MapaFoliumSona9.py
+++ master/src/2_MapaFoliumSona9.py
@@ -4,12 +4,49 @@ Created on Sat May 16 12:34:26 2020
 
 @author: Carles
 """
+import sys
+sys.path.insert(0,'folium')
 
-import os
-os.chdir("C:/Users/Carles/Desktop/DatathonGithub/Sona9-2020/master")
+import folium
+import folium.plugins
 import folium
 import pandas as pd
 from conf import paths
+from folium.features import *
+
+
+
+import os
+os.chdir("C:/Users/Carles/Desktop/DatathonGithub/Sona9-2020/master")
+         
+class DivIcon(MacroElement):
+    def __init__(self, html='', size=(30,30), anchor=(0,0), style=''):
+        """TODO : docstring here"""
+        super(DivIcon, self).__init__()
+        self._name = 'DivIcon'
+        self.size = size
+        self.anchor = anchor
+        self.html = html
+        self.style = style
+        self._template = Template(u"""
+            {% macro header(this, kwargs) %}
+              <style>
+                .{{this.get_name()}} {
+                    {{this.style}}
+                    }
+              </style>
+            {% endmacro %}
+            {% macro script(this, kwargs) %}
+                var {{this.get_name()}} = L.divIcon({
+                    className: '{{this.get_name()}}',
+                    iconSize: [{{ this.size[0] }},{{ this.size[1] }}],
+                    iconAnchor: [{{ this.anchor[0] }},{{ this.anchor[1] }}],
+                    html : "{{this.html}}",
+                    });
+                {{this._parent.get_name()}}.setIcon({{this.get_name()}});
+            {% endmacro %}
+            """)
+
 
 def main():
     ############### Main Paths #########################
@@ -37,21 +74,43 @@ def main():
     for index, row in df.iterrows():
            tooltip = row['NomdelGrup']
            icon = folium.features.CustomIcon(icon_url,icon_size=(28, 30))
-           html=""" <h1> """+row['NomdelGrup']+"""</h1><br>
-                    Estil: """+ row['Estil'] + """
-                    <p>
-                    <code>Youtube Video: <a href="url">"""+row['URLvideoYoutube']+"""</a>
-                        
-                    </code>
-                    </p>
-                    """
+           html="<h1> "+row['NomdelGrup']+"</h1><br> Estil: "+ row['Estil'] + \
+                    "<p><code>Youtube Video: <a href='url'>"+row['URLvideoYoutube']+"</a>"+\
+                    "</code></p>"
+                    
            iframe = folium.IFrame(html=html, width=400, height=150)
            popup = folium.Popup(iframe, max_width=1000)
     
            folium.Marker((row['lat'], row['lon']),           
                    popup=popup,
                    tooltip=tooltip,
-                   icon=icon).add_to(map_)
+                   icon=icon).add_to(map_),
+           folium.Marker(
+                    (row['lat']-0.0001, row['lon']),
+                    icon=DivIcon(
+                    size=(25,20),
+                    anchor=(25,0),
+                    html= "<b>"+ row['NomdelGrup'] +"</b>",
+                    style="""
+                        font-size:12px;
+                        color:black;
+                        background-color: transparent;
+                        border-color: transparent;
+                        text-align: right;
+                        """
+                )).add_to(map_)
+           
+           legend_html = """
+                         <div style="position: fixed; 
+                         bottom: 50px; right: 50px; width: 100px; height: 60px; 
+                         border:2px solid grey; z-index:9999; font-size:14px;
+                         background-color:white;
+                         ">&nbsp; <b>Llegenda</b> <br>
+                         &nbsp; Grup &nbsp; <img src="https://img.icons8.com/windows/32/000000/progressive-rock.png"><br>
+                          </div>
+                         """
+           map_.get_root().html.add_child(folium.Element(legend_html))
+           
     #3. Saving the Result Map in the output path
     map_.save(OutputMap_Path)
     return None
